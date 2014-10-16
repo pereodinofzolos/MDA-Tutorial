1adcac1de4d2c0724f055b15f499f7117606dfd2
pereodinofzolos <edem4420@hotmail.com>
2014-10-16 00:57:26 -0400

    FirstClassEdem.cpp - First Commit

---
+++ FirstClassEdem.cpp
@@ -0,0 +1,12 @@
+#Include<iostream>
+#include<Math.h>
+
+Class FirstCLassEdem {
+  int FirstNumber, SecondNumber;
+  public double RandomFunction (int Parameter1, int Parameter2);
+} FirstUse
+  
+
+double FirstClass::RandomFunction (int AddValue, int MultipValue){
+  return ( (3.14159)*(MultipValue + AddValue)/(9.51413) );
+}
